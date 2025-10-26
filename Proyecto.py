ddb14b3a9e0b3fc27b872343fc97035929882ad2
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-26 14:59:20 -0500

    Agregar endpoint PUT /proyecto/{id} para actualizar proyectos

--- Proyecto.py
+++ Proyecto.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from Database import SessionDep
 from models import Proyecto, ProyectoCreate, Estado
 from typing import List
@@ -19,3 +19,16 @@ async def lista_proyectos(presupuesto: float, estado: Estado, session: SessionDe
     query = select(Proyecto).where(Proyecto.presupuesto.contains(presupuesto), Proyecto.estado == estado)
     proyectos = session.exec(query).all()
     return proyectos
+
+@router.put("/{proyecto_id}", response_model=Proyecto)
+async def update_proyecto(proyecto_id: int, updated: ProyectoCreate, session: SessionDep):
+    proyecto =session.get(Proyecto, proyecto_id)
+    if not proyecto:
+        raise HTTPException(status_code=404, detail="Proyecto no encontrado")
+    proyecto.nombre = updated.nombre
+    proyecto.descripcion = updated.descripcion
+    proyecto.presupuesto = updated.presupuesto
+    proyecto.estado = updated.estado
+    session.commit()
+    session.refresh(proyecto)
+    return proyecto
