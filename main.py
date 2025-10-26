ad047126a903b069584c3d3f6e9c1adcffaed9f8
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-26 14:31:42 -0500

    Llamar create_tables() para generar tablas en SQLite

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
+from Database import create_tables
 import Empleado, Proyecto
 
 app = FastAPI(title="Sistema de Gestion de Proyectos")
+create_tables()
 
 app.include_router(Empleado.router)
 app.include_router(Proyecto.router)
