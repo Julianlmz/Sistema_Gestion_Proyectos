4983799fd12b4aadab0cac270945c3dabde5490e
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-26 13:05:55 -0500

    Registro el router de empleados en la aplicación principal

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
+import Empleado
 
-app = FastAPI()
+app = FastAPI(title="Sistema de Gestion de Proyectos")
 
+app.include_router(Empleado.router)
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
