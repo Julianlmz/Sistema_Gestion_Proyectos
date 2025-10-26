a6d13451aeeb67b72730cf4ed828c0d36da5e1d9
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-26 14:13:34 -0500

    Agregar GET /empleado filtrados por especialidad y estado

--- Empleado.py
+++ Empleado.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from Database import SessionDep
-from models import Empleado, EmpleadoCreate
+from models import Empleado, EmpleadoCreate, Estado
+from typing import List
+from sqlmodel import select
 
 router = APIRouter(tags=["Empleado"], prefix="/empleado")
 
@@ -11,3 +13,9 @@ async def create_empleado(new_empleado: EmpleadoCreate, session: SessionDep):
     session.commit()
     session.refresh(empleado)
     return empleado
+
+@router.get("/", response_model=List[Empleado])
+async def lista_empleados(especialidad: str, estado: Estado, session: SessionDep):
+    query = select(Empleado).where(Empleado.especialidad.contains(especialidad), Empleado.estado == estado)
+    empleados = session.exec(query).all()
+    return empleados
