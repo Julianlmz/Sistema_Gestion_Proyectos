d3e24bbfaf6ae283fc12e6beaa73a63b00d69aaa
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-26 14:42:36 -0500

    Agregar GET /empleado filtrados por presupuesto y estado

--- Proyecto.py
+++ Proyecto.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from Database import SessionDep
-from models import Proyecto, ProyectoCreate
+from models import Proyecto, ProyectoCreate, Estado
+from typing import List
+from sqlmodel import select
 
 router = APIRouter(tags=["Proyecto"], prefix="/proyecto")
 
@@ -11,3 +13,9 @@ async def create_proyecto(new_proyecto: ProyectoCreate, session: SessionDep):
     session.commit()
     session.refresh(proyecto)
     return proyecto
+
+@router.get("/", response_model=List[Proyecto])
+async def lista_proyectos(presupuesto: float, estado: Estado, session: SessionDep):
+    query = select(Proyecto).where(Proyecto.presupuesto.contains(presupuesto), Proyecto.estado == estado)
+    proyectos = session.exec(query).all()
+    return proyectos
