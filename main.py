7a00a8fd3ca3cde13ff56c0461f0a756686d7256
Julianlmz <jsleal77@ucatolica.edu.co>
2025-10-26 13:55:36 -0500

    Registro el router de Proyectos en el main

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-import Empleado
+import Empleado, Proyecto
 
 app = FastAPI(title="Sistema de Gestion de Proyectos")
 
 app.include_router(Empleado.router)
+app.include_router(Proyecto.router)
 
 @app.get("/")
 async def root():
